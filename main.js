09b318d50299e7a15d122ea33a141988f88dc478
Thea <thea.mushambadze@gmail.com>
2019-10-06 00:54:45 +0400

    add date

---
+++ main.js
@@ -0,0 +1,9 @@
+var d = new Date();
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!");
+
+
+
+
