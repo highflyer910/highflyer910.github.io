5b36f0573eca4cdecc9d7de09a53041f3fc8db46
Thea <thea.mushambadze@gmail.com>
2020-07-09 21:33:23 +0400

    wear your mask!

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var d = new Date();
 
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-document.getElementById('wday').innerHTML = ("No tracking. Happy " + days[d.getDay()] + "!");
+document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "! Wear a facemask.");
 
 
 
