bfe03e54ec7df00320ad40fe84eeebb9e4e1884a
highflyer910 <tmush2014@agruni.edu.ge>
2021-07-27 20:42:29 +0400

    add cursor

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!<br
 
 
 
+const mouse = document.querySelector('.mouse');
+document.addEventListener('mousemove', e => {
+	let {clientX: x, clientY: y} = e;
 
+	mouse.style.transform = `translate(
+		${x - mouse.offsetWidth / 2}px,
+		${y - mouse.offsetHeight / 2}px
+	)`;
+	
+});
