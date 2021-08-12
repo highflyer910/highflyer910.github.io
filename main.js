5b00a696ee6104fb15917f1128bc6edf5917a945
highflyer910 <tmush2014@agruni.edu.ge>
2021-08-13 00:49:03 +0400

    add text

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var d = new Date();
 
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!<br><br> Wear a Mask. Save Lives.");
+document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!<br><br>Please, get vaxxed, and wear a mask. Save Lives.");
 
 
 
