8b2d6e09b47ce00eb49532537b8395602b1ab87a
Thea <thea.mushambadze@gmail.com>
2022-01-28 06:40:20 +0400

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var d = new Date();
-
-//var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!<br><br>Please, wear a mask. Save Lives.");
 
