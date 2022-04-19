007714b42eccd88c8ec3a764b3e9c95a9083b977
Thea <thea.mushambadze@gmail.com>
2022-04-19 21:48:08 +0400

    update

--- main.js
+++ main.js
@@ -4,18 +4,6 @@ var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
 document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!");
 
 
-
-const mouse = document.querySelector('.mouse');
-document.addEventListener('mousemove', e => {
-	let {clientX: x, clientY: y} = e;
-
-	mouse.style.transform = `translate(
-		${x - mouse.offsetWidth / 2}px,
-		${y - mouse.offsetHeight / 2}px
-	)`;
-	
-});
-
 gsap.to('.mask', {
   duration: 6,
   ease: "sine.out",
