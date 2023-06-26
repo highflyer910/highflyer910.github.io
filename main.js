caffffb367c79a14a36966b84ab8aaf889f0162b
Thea Mushambadze <thea.mushambadze@gmail.com>
2023-06-27 02:47:22 +0400

    update

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var encEmail = "bXVzaGFtYmFkemV0ZW9uYUBnbWFpbC5jb20=";
 const form = document.getElementById("contact");
 
+form.setAttribute("href", "mailto:".concat(atob(encEmail)));
+
 var d = new Date();
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 
-form.setAttribute("href", "mailto:".concat(atob(encEmail)));
-
-
 document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!");
 
-
+/** 
 gsap.to('.mask', {
   duration: 6,
   ease: "sine.out",
   "--mask": 'linear-gradient(-30deg, transparent -50%, black 0%)'
 })
+
+*/
